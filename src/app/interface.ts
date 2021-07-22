9466cf8eb64434cca00665ec3432730c915bcdbe
Monica Hackney <mhackney@HAGERTY.com>
2021-07-22 19:27:00 -0400

    interface.ts updates

--- src/app/interface.ts
+++ src/app/interface.ts
@@ -5,11 +5,26 @@ export interface Park {
     fullName:string;
     description:string;
     activities:Activities[];
+    city: string;
+    stateCode: string;
     states:string;
     id:string;
     parkCode:string;
+    standardHours:{
+        monday: string;
+        tuesday: string;
+        wednesday: string;
+        thursday: string;
+        friday: string;
+        saturday: string;
+        sunday: string;
+    }
 }
 
 export interface Activities {
     name:string;
+}
+
+export interface Webcams {
+    url: string;
 }
