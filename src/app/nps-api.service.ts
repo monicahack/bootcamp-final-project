a1e128288dfd38f8f402160d64a2df34b3b308a0
Kelsey Kruzona <kelsey@fusionary.com>
2021-07-23 16:34:04 -0400

    updated parksURL in order to get limit increased to show all results

--- src/app/nps-api.service.ts
+++ src/app/nps-api.service.ts
@@ -7,19 +7,20 @@ import { ParkResponse, Park } from './interface';
   providedIn: 'root'
 })
 export class NpsApiService {
-  parksURL = 'https://developer.nps.gov/api/v1/parks?api_key=BazAUNaZgkG8JZqeWIj3HTckfHrQsIfrRy2B2daq';
-  parkDetailURL ='https://developer.nps.gov/api/v1/parks'
+  parksURL = 'https://developer.nps.gov/api/v1/parks';
+  parkDetailURL ='https://developer.nps.gov/api/v1/parks';
   activitiesURL = 'https://developer.nps.gov/api/v1/activities?api_key=BazAUNaZgkG8JZqeWIj3HTckfHrQsIfrRy2B2daq';
   amenitiesURL = 'https://developer.nps.gov/api/v1/amenities?api_key=BazAUNaZgkG8JZqeWIj3HTckfHrQsIfrRy2B2daq';
   eventsURL = 'https://developer.nps.gov/api/v1/events?api_key=BazAUNaZgkG8JZqeWIj3HTckfHrQsIfrRy2B2daq';
   thingsToDoURL = 'https://developer.nps.gov/api/v1/thingstodo?api_key=BazAUNaZgkG8JZqeWIj3HTckfHrQsIfrRy2B2daq';
-  webcamsURL = 'https://developer.nps.gov/api/v1/webcams?api_key=BazAUNaZgkG8JZqeWIj3HTckfHrQsIfrRy2B2daq'
+  webcamsURL = 'https://developer.nps.gov/api/v1/webcams?api_key=BazAUNaZgkG8JZqeWIj3HTckfHrQsIfrRy2B2daq';
   api_key = 'BazAUNaZgkG8JZqeWIj3HTckfHrQsIfrRy2B2daq';
+  limit = '467';
   
   constructor(public client: HttpClient) { }
   
   getParks() {
-    let parksURL = this.parksURL;
+    let parksURL = `${this.parksURL}?limit=${this.limit}&api_key=${this.api_key}`;
     return this.client.get<ParkResponse>(parksURL);
   }
 
