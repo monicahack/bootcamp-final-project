364515e7f04aae4bc2d6faebe0322500999aa509
Morgan Haskins <morgan.haskins@fusionary>
2021-07-22 21:24:37 -0400

    Landing page progress

--- src/app/app-landing-page/app-landing-page.component.ts
+++ src/app/app-landing-page/app-landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NpsApiService } from '../nps-api.service';
+import { FavoriteService } from '../favorite.service';
 
 @Component({
   selector: 'app-app-landing-page',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppLandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public api: NpsApiService, public tree: FavoriteService) { }
 
   ngOnInit(): void {
   }
