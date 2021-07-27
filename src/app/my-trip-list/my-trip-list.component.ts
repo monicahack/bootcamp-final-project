135948f091b6c04a0d41995bcbd8117a1f790b20
Monica Hackney <mhackney@HAGERTY.com>
2021-07-26 21:02:59 -0400

    progress on trip list component page

--- src/app/my-trip-list/my-trip-list.component.ts
+++ src/app/my-trip-list/my-trip-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NpsApiService } from '../nps-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { FavoriteService } from '../favorite.service';
+import { faTree } from '@fortawesome/free-solid-svg-icons';
+import { Park } from '../interface';
 
 @Component({
   selector: 'app-my-trip-list',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-trip-list.component.css']
 })
 export class MyTripListComponent implements OnInit {
-
-  constructor() { }
+  park: Park [] = [];
+  favoriteList: Park[] = [];
+  faTree = faTree;
+  constructor(public api:NpsApiService, private route:ActivatedRoute, public favorite: FavoriteService) { }
 
   ngOnInit(): void {
-  }
+    this.favoriteList=this.favorite.getFavorites();
+}
 
+toggleFavorite(park: Park) {
+  this.favorite.toggleFavorite(park);
+  }
 }
