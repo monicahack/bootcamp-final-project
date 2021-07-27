2bb23244c81ba501f67355519abf31b8398d9326
Nicole Choponis <nicolechoponis@gmail.com>
2021-07-26 21:52:52 -0400

    park-detail

--- src/app/park-list/park-list.component.ts
+++ src/app/park-list/park-list.component.ts
@@ -10,12 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ParkListComponent implements OnInit {
   parkCode: string = '';
+  stateCode: string = '';
   park: any = {data:[]};
   constructor(public api:NpsApiService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.stateCode = params['stateCode'];
     this.api.getParkList().subscribe((data) => {
       this.park = data;
       console.log(this.park.data);
     });
-  }}
+  });
+}}
