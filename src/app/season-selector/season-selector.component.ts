e8b1ebf77910835862bf84aafdaf47117006a77d
Kelsey Kruzona <kelsey@fusionary.com>
2021-07-26 21:26:59 -0400

    added seasons but might not need

--- src/app/season-selector/season-selector.component.ts
+++ src/app/season-selector/season-selector.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NpsApiService } from '../nps-api.service';
+import { ActivatedRoute } from '@angular/router';
+import{ ThingsToDo } from '../interface';
+
+interface SeasonsListing {
+  seasonType: string;
+}
 
 @Component({
   selector: 'app-season-selector',
@@ -6,8 +13,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./season-selector.component.css']
 })
 export class SeasonSelectorComponent implements OnInit {
+  seasons: SeasonsListing[] = [
+    {
+      seasonType: "Spring"
+    },
+    {
+      seasonType: "Summer"
+    },
+    {
+      seasonType: "Winter"
+    },
+    {
+      seasonType: "Fall"
+    }
+  ];
 
-  constructor() { }
+  season: string[] = [];
+
+  constructor(public api:NpsApiService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
   }
