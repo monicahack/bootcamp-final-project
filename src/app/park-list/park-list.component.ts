84140381806844da9165dd4f93f18b41379d458b
Nicole <nbush89@gmail.com>
2021-07-27 19:19:10 -0400

    got park-list to load only parks from state clicked

--- src/app/park-list/park-list.component.ts
+++ src/app/park-list/park-list.component.ts
@@ -1,46 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { NpsApiService } from '../nps-api.service';
-import{ Park } from '../interface';
+import { Park } from '../interface';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-park-list',
   templateUrl: './park-list.component.html',
-  styleUrls: ['./park-list.component.css']
+  styleUrls: ['./park-list.component.css'],
 })
 export class ParkListComponent implements OnInit {
   parkCode: string = '';
   stateCode: string = '';
-  park: any = {data:[]};
-  constructor(public api:NpsApiService, private route:ActivatedRoute) { }
+  park: any = { data: [] };
+  filteredByState: any;
+  constructor(public api: NpsApiService, private route: ActivatedRoute) {}
 
   queryParam: string = '';
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params=>{
+    this.route.queryParams.subscribe((params) => {
       this.stateCode = params['stateCode'];
-      console.log(params.stateCode);
-      console.log(this.stateCode);
-      // return this.api.getParkList();
-    //   this.api.getParks().subscribe((data) => {
-    //   this.park = data;
-    //   console.log(this.park.data);
-    // });
+      this.api.getParksByState(this.stateCode).subscribe((states: any) => {
+        this.setFilteredStates(states.data);
+        console.log(this.filteredByState);
+      });
+      this.api.getParks().subscribe((data) => {
+        this.park = data.data;
+      });
+      console.log(this.park);
     });
-
-// this.route.params.subscribe((params)=>{
-    //   this.stateCode = params['stateCode'];
-    //   console.log(params);
-    //   // this.stateCode = params.stateCode;
-    //   console.log(this.stateCode);
-    // })
-    //this needs to communicate with the NPS API service
-    this.route.params.subscribe((params) => {
-      this.stateCode = params['stateCode'];
-    this.api.getParks().subscribe((data) => {
-      this.park = data;
-      console.log(this.park.data);
-    });
-  });
-}}
-
+  }
+  setFilteredStates(stateArray: any[]) {
+    this.filteredByState = stateArray;
+  }
+}
