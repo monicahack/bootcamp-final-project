e873fcd1b3cfb40f0b72ddb2ea43f93860b5562d
Monica Hackney <mhackney@HAGERTY.com>
2021-08-09 19:35:54 -0400

    remove season selector component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { AppLandingPageComponent } from './app-landing-page/app-landing-page.com
 import { ParkDetailPageComponent } from './park-detail-page/park-detail-page.component';
 import { StatesListComponent } from './states-list/states-list.component';
 import { ParkListComponent } from './park-list/park-list.component';
-import { SeasonSelectorComponent } from './season-selector/season-selector.component';
 import { RandomParkGeneratorComponent } from './random-park-generator/random-park-generator.component';
 import { MyTripListComponent } from './my-trip-list/my-trip-list.component';
 
@@ -12,7 +11,6 @@ const routes: Routes = [
   {path: 'park-detail/:parkCode', component: ParkDetailPageComponent},
   {path: 'states-list', component: StatesListComponent},
   {path: 'park-list', component: ParkListComponent},
-  {path: 'season-selector', component: SeasonSelectorComponent},
   {path: 'random-park-generator', component: RandomParkGeneratorComponent},
   {path: 'my-trip-list', component: MyTripListComponent},
 
